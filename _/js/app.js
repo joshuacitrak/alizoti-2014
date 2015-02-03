648727d955b2a76da5a6bb42656b84351ab68ed8
joshuacitrak <joshua.citrak@apollo.edu>
2015-02-03 15:36:23 -0800

    added ng routing, partials

---
+++ _/js/app.js
@@ -0,0 +1,37 @@
+(function() {
+	var app = angular.module('alizotiApp', [ 'ngRoute', 'ngAnimate' ]).config(
+			router);
+
+	function router($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl : 'partials/home.html'
+		}).when('/home', {
+			templateUrl : 'partials/home.html'
+		}).when('/services/:servicesId', {
+			templateUrl : 'partials/services.html'
+		}).when('/contact', {
+			templateUrl : 'partials/contact.html'
+		}).otherwise({
+			redirectTo : '/'
+		});
+	}
+	;
+
+	var dataObj = [];
+
+	app.controller('PageController', function($scope, $http) {
+		$http.get('_/js/data.json').success(
+				function(data, status, headers, config) {
+					$scope.dataObj = data;
+				}).error(function(data, status, headers, config) {
+			alert('data could not be loaded.');
+		});
+	});
+
+	app.controller('NavController', function($scope, $location) {
+		this.isActive = function(viewLocation)
+		{
+			return viewLocation === $location.path();		
+		};
+	});
+})();
